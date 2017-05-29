6a558a23f85e68adfeb4579cb73d8f709d46c9df
hector <hdavila@thoughtworks.com>
2017-05-29 10:35:23 -0500

    [#2] update menu.test to verify the number of MenuItems

--- src/components/menu/menu.test.js
+++ src/components/menu/menu.test.js
@@ -2,6 +2,7 @@ import React from 'react';
 import { shallow } from 'enzyme';
 import Menu from './menu';
 
+<<<<<<< HEAD
 test('should render Menu view without crashing', () => {
     shallow(<Menu />);
 });
@@ -9,4 +10,9 @@ test('should render Menu view without crashing', () => {
 test('should render Menu view has a NavBar item', () => {
     const wrapper = shallow(<Menu />);
     expect(wrapper.find('Navbar').length, 1)
+=======
+it('should has two menu item', () => {
+    const menu = shallow(<Menu/>);
+    expect(menu.find(MenuItem)).toHaveLength(2);
+>>>>>>> c729236... [@hdavila][2]update menu.test to verify the number of MenuItems
 });
