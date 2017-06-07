04f88dac44b80e1580ae43cd62f70364374b08f2
Schubert D. Rodríguez <scrodrig@thoughtworks.com>
2017-06-07 11:53:27 -0500

    [#8] with @xnaunay - The UI handle the error response and return an error object

--- src/services/PeopleService.js
+++ src/services/PeopleService.js
@@ -1,9 +1,9 @@
 import axios from 'axios';
 
 export default class PeopleService {
-
   get(id) {
     return axios.get('/people/' + id)
-    .then(response => {return response.data[0]});
+    .then(response => {return response.data[0]})
+    .catch(error =>	Promise.reject(error));    
   }
 }
--- src/services/PeopleService.test.js
+++ src/services/PeopleService.test.js
@@ -2,16 +2,7 @@ import PeopleService from './PeopleService';
 import axios from 'axios';
 
 describe('PeopleService', () => {
-  it('should get person information', () => {
-    let expected = {data: [Math.random()]};
-    axios.get = jest.fn(() => new Promise(resolve => resolve(expected)));
-    let peopleService = new PeopleService();
-
-    let personPromise = peopleService.get(1);
-
-    return expect(personPromise).resolves.toBe(expected.data[0]);
-  });
-
+  
   it('should call correct endpoint', () => {
     const personId = Math.random();
     const expected = {data: [Math.random()]};
@@ -22,4 +13,23 @@ describe('PeopleService', () => {
 
     expect(axios.get).toHaveBeenCalledWith('/people/' + personId);
   });
+
+  it('should get person information', () => {
+    const personId = Math.random();
+    let expected = {data: [Math.random()]};
+    axios.get = jest.fn(() => new Promise(resolve => resolve(expected)));
+    let peopleService = new PeopleService();
+    let personPromise = peopleService.get(personId);
+    return expect(personPromise).resolves.toBe(expected.data[0]);
+  });
+
+  it('should return an error object when an error ocurred in server', () => {
+    const personId = Math.random();
+    const expected = {status: Math.random()};
+    axios.get = jest.fn(() => new Promise((resolve, reject) => reject(expected)));
+    let peopleService = new PeopleService();
+    let personPromise = peopleService.get(personId);
+    return expect(personPromise).rejects.toEqual(expected);
+  });
+
 });
