36da650787b6ca7ce6108e612f3c029cc358452c
hector <hdavila@thoughtworks.com>
2017-06-08 16:47:18 -0500

    [#6] with @smolina and @xnaunay - removing medicalLeave and vacation components

--- src/index.js
+++ src/index.js
@@ -4,8 +4,6 @@ import { BrowserRouter, Route } from 'react-router-dom';
 
 import App from './components/app/app';
 import Home from './components/home/home';
-import MedicalLeave from './components/medicalLeave/medicalLeave';
-import Vacation from './components/vacation/vacation';
 import registerServiceWorker from './registerServiceWorker';
 import axios from 'axios';
 
@@ -20,8 +18,6 @@ ReactDOM.render(
     <BrowserRouter >
         <App>
             <Route exact path="/" component={Home} />
-            <Route path="/request/vacation" component={Vacation} />
-            <Route path="/request/medical-leave" component={MedicalLeave} />
         </App>
     </BrowserRouter>
     , document.getElementById('root'));
