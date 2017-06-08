2fb2a8f5024f726b4a70fc81a743d9e708036d5e
Elizabeth Velasco <evelasco@thoughtworks.com>
2017-06-08 14:48:59 -0500

    with @cortiz - Delete medical leave and vacation folders

--- src/components/medicalLeave/medicalLeave.test.js
+++
@@ -1,7 +0,0 @@
-import React from 'react';
-import { shallow } from 'enzyme';
-import MedicalLeave from './medicalLeave';
-
-test('should render MedicalLeave without crashing', () => {
-    shallow(<MedicalLeave />);
-});
--- src/components/vacation/vacation.test.js
+++
@@ -1,7 +0,0 @@
-import React from 'react';
-import { shallow } from 'enzyme';
-import Vacation from './vacation';
-
-test('should render Vacation view without crashing', () => {
-    shallow(<Vacation />);
-});
