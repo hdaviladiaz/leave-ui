78e439a5b260a229bb130367636226de5d4d9a0f
Elizabeth Velasco <evelasco@thoughtworks.com>
2017-06-09 10:09:52 -0500

    [#5] with @cortiz - Test that PersonalInformation component renders correctly

--- src/components/home/resources/personal-information.test.js
+++ src/components/home/resources/personal-information.test.js
@@ -1,11 +1,20 @@
 import React from 'react';
 import { shallow } from 'enzyme';
 import PersonalInformation from './personal-information';
+import renderer from 'react-test-renderer';
 
 describe('PersonalInformation', () => {
 
-  it('should render PersonalInformation without crashing', () => {
-    shallow(<PersonalInformation />);
+  const vivi = {"employeeId":"15576",
+                "preferredName":"Viviana Perez",
+                "picture":{"url":"https://jigsaw.thoughtworks.net/consultants/22118/show_picture?style=profile"},
+                "hireDate":"2013-08-05",
+                "homeOffice":{"name":"Quito"},
+                "workingOffice":{"name":"Pune"}}
+
+  it('should render without crashing', () => {
+    const wrapper = shallow(<PersonalInformation person={vivi} error={false} />);
+    expect(wrapper).toMatchSnapshot();
   });
 
 });
