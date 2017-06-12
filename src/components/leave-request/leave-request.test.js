517ac0191bdf26abbcbb09b89a0fb3d4f3835c5f
Carlos Ortiz <cortiz@thoughtworks.com>
2017-06-12 18:04:22 -0500

    [#5] with @smolina - Refactor initial test with shallow

--- src/components/leave-request/leave-request.test.js
+++ src/components/leave-request/leave-request.test.js
@@ -1,13 +1,13 @@
 'use strict';
 import React from 'react';
-import { mount } from 'enzyme';
+import { shallow } from 'enzyme';
 import LeaveRequest from './leave-request.js';
 
 describe('LeaveRequest', () => {
 
   it('should render LeaveRequest component', () => {
-    const wrapper = mount(<LeaveRequest />);
-    const inst = wrapper.instance();
-    expect(inst).toBeInstanceOf(LeaveRequest);
+    const wrapper = shallow(<LeaveRequest />);
+    const instanceLeaveRequest = wrapper.instance();
+    expect(instanceLeaveRequest).toBeInstanceOf(LeaveRequest);
   });
 });
