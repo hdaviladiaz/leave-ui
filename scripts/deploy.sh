fba6ce4ba9d6c72d3bba9e55879b4eb5b6849948
SamMolina <smolina@thoughtworks.com>
2017-06-12 11:26:45 -0500

    [#6] with @xnaunay - Add API_URL in deploy.sh

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,5 +1,6 @@
 heroku maintenance:on --app "$1"
 git remote rm heroku
+heroku config:set API_URL="https://$2.herokuapp.com"
 heroku git:remote -a "$1"
 git push -f heroku master
 heroku maintenance:off --app "$1"
