cb2a3341431dc66bed7bcd309e5f5a4369301b7e
Carlos Ortiz <cortiz@thoughtworks.com>
2017-06-12 15:53:15 -0500

    [#5] with @smolina - first test to render component LeaveRequest

--- src/components/leave-request/leave-request.test.js
+++ src/components/leave-request/leave-request.test.js
@@ -1,10 +1,13 @@
+'use strict';
 import React from 'react';
-import { shallow } from 'enzyme';
+import { mount } from 'enzyme';
 import LeaveRequest from './leave-request.js';
 
 describe('LeaveRequest', () => {
 
-  it('should render LeaveRequest without crashing', () => {
-    shallow(<LeaveRequest />);
+  it('should render LeaveRequest component', () => {
+    const wrapper = mount(<LeaveRequest />);
+    const inst = wrapper.instance();
+    expect(inst).toBeInstanceOf(LeaveRequest);
   });
 });
