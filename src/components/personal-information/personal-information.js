739e88dbb614d3de9b4e86abec2599f20dd5115d
Yadira <ymarin@thoughtworks.com>
2017-06-20 11:15:24 -0500

    [#23] with @evelasco - Delete unnecessary data from personal-information component

--- src/components/personal-information/personal-information.js
+++ src/components/personal-information/personal-information.js
@@ -1,52 +1,34 @@
 import React from 'react';
 import { Alert } from 'react-bootstrap';
+import './personal-information.css';
 
 const PersonalInformation = ({person, error}) =>{
 
   if(error){
     return(
-		      <Alert bsStyle="danger">
-		      Hubo un error al consultar su información
-		      </Alert>);
+      <Alert bsStyle="danger">
+        Hubo un error al consultar su información
+      </Alert>);
   }
 
   if(!person){
    return(
-		   <Alert bsStyle="info">
-		   Recuperando información
-		   </Alert>);
+	   <Alert bsStyle="info">
+      Recuperando información
+	   </Alert>);
   }
 
   return(
-    <div>
-    <h3 className="home-personal-information">Tu Información</h3>
-    <div className="row">
-        <div className="col-lg-2">
-            <img src={person.picture.url} alt="foto personal"/>
-        </div>
-        <div className="col-lg-10">
-            <dl className="dl-horizontal">
-                <dt>Nombre</dt>
-                <dd>{person.preferredName}</dd>
-                <dt>Correo Electrónico</dt>
-                <dd>{ person.loginName }@thoughtworks.com</dd>
-                <dt>Oficina base</dt>
-                <dd>{person.homeOffice.name}</dd>
-                <dt>Oficina actual</dt>
-                <dd>{person.workingOffice.name}</dd>
-                <dt>Fecha de contratación</dt>
-                <dd>{person.hireDate}</dd>
-                <dt>Días disponibles</dt>
-                <dd></dd>
-            </dl>
-        </div>
-    </div>
+    <div className="personal-container col-lg-12">
+      <div className="col-lg-3 no-padding">
+        <img src={person.picture.url} alt="foto personal" className="img-picture"/>
+      </div>
+      <div className="col-lg-9">
+        <p>{person.preferredName}</p>
+        <p>{person.hireDate}</p>
+      </div>
     </div>
   );
-
-
 };
-
-
 
 export default PersonalInformation;
