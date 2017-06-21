6773e321b0147bc2c488599102d4cdec5c0d9505
Yadira <ymarin@thoughtworks.com>
2017-06-21 11:05:03 -0500

    [#23] - Checking active navigations links

--- src/components/navigation/navigation.js
+++ src/components/navigation/navigation.js
@@ -1,17 +1,28 @@
 import React, {Component} from 'react';
-import {Link} from 'react-router-dom'
-import './navigation.css'
+import {Link} from 'react-router-dom';
+import PropTypes from 'prop-types';
+import './navigation.css';
 
 class Navigation extends Component {
+
+  checkActive(href) {
+    var isActive = this.context.router.route.location.pathname === href;
+    return isActive ? 'disabled' : '';
+  }
+
   render() {
     return (
       <div className="col-lg-12">
-         <div><Link className="btn btn-default navigation-button" to="/">Mis Vacaciones</Link></div>
-         <div><Link className="btn btn-default navigation-button" to="/admin">Solicitudes</Link></div>
+         <div><Link className={`btn btn-default navigation-button ${this.checkActive("/")}`} to="/">Mis Vacaciones</Link></div>
+         <div><Link className={`btn btn-default navigation-button ${this.checkActive("/admin")}`} to="/admin">Solicitudes</Link></div>
          <div><a className="btn btn-default navigation-button" >Cerrar Sesion</a></div>
       </div>
     );
   }
 }
 
-export default Navigation
+Navigation.contextTypes = {
+  router: PropTypes.object
+};
+
+export default Navigation;
