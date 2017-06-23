6aafe2075da31fb6059f795aa863cc41233e9297
Schubert D. Rodríguez <scrodrig@thoughtworks.com>
2017-06-23 10:57:34 -0500

    [#29] with @rcordova - Add configuration in NewLeaveRequest in order to use the calendar

--- src/components/new-leave-request/new-leave-request.test.js
+++ src/components/new-leave-request/new-leave-request.test.js
@@ -2,7 +2,7 @@ import React from 'react';
 import NewLeaveRequest from './new-leave-request';
 import renderer from 'react-test-renderer';
 
-xit('should render NewLeaveRequest as snapshot', () => {
+it('should render NewLeaveRequest as snapshot', () => {
   const tree = renderer.create(<NewLeaveRequest/>).toJSON();
   expect(tree).toMatchSnapshot();
 });
