deb502cd0d014821af432602348ce2793ed9961e
Schubert D. Rodríguez <scrodrig@thoughtworks.com>
2017-06-27 14:55:04 -0500

    [#30] with @evelasco - add a component to select who will approve new leave request

--- src/components/new-leave-request/new-leave-request.js
+++ src/components/new-leave-request/new-leave-request.js
@@ -2,9 +2,14 @@ import React, { Component } from 'react';
 import { Panel, Col } from 'react-bootstrap';
 import LeaveCalendar from '../leave-calendar/leave-calendar';
 import LeaveRequestService from '../../services/leaveRequestService'
+import PeopleService from '../../services/peopleService';
 import './new-leave-request.css';
 import AlertContainer from 'react-alert';
 import moment from 'moment';
+import Select from 'react-select';
+import 'react-select/dist/react-select.css';
+import _ from 'underscore';
+
 
 export default class NewLeaveRequest extends Component {
 
@@ -29,13 +34,20 @@ export default class NewLeaveRequest extends Component {
       this.state = {
         dateFrom: moment(),
         dateTo: moment(),
-        haveInformed: false
+        haveInformed: false,
+        options: []
       },
-      this.saveLeaveRequest = this.saveLeaveRequest.bind(this);
+    this.peopleService = PeopleService.getInstance();
+    this.saveLeaveRequest = this.saveLeaveRequest.bind(this);
     this.toggleInformed = this.toggleInformed.bind(this);
     this.leaveRequestService = LeaveRequestService.getInstance();
     this.showAlert = this.showAlert.bind(this);
+    this.onSelectChange = this.onSelectChange.bind(this);
   }
+
+  componentDidMount() {
+   this.peopleService.getOfficePeople().then(people => this.setState({options: this.mapPeopleToList(people)})).catch(error => this.setState({options: []}));
+ }
 
   handleDateFrom(dateFrom) {
     this.setState({ dateFrom: dateFrom });
@@ -50,20 +62,27 @@ export default class NewLeaveRequest extends Component {
     this.setState({ haveInformed: !this.state.haveInformed })
   }
 
+  onSelectChange(val) {
+    this.setState({approvalPerson: val});
+  }
+
+  mapPeopleToList = (people) => {
+   return _.map(people, function(person) {
+       return {
+         value: person.loginName +'@thoughtworks.com',
+         label: person.preferredName + ' (' + person.loginName + '@thoughtworks.com' + ')'
+       }
+   });
+  }
+
   saveLeaveRequest() {
     if (!this.state.haveInformed) {
       this.showAlert('Por favor informe a su equipo y su PM y seleccione la opción.');
       return;
-    }
-
-    if (this.state.dateFrom >= this.state.dateTo) {
-      this.showAlert('La fecha de inicio no puede ser mayor o igual a la fecha de retorno.');
-      return;
-
-    }
+    }   
 
     this.leaveRequestService
-      .createLeaveRequest({ start_date: this.state.dateFrom, end_date: this.state.dateTo, return_date: this.state.dateTo })
+      .createLeaveRequest({ start_date: this.state.dateFrom, end_date: this.state.dateTo, return_date: this.state.dateTo, approver_id: this.state.approvalPerson.value })
       .then(
       function (response) {
         window.location = "/dashboard/leaves";
@@ -94,8 +113,7 @@ export default class NewLeaveRequest extends Component {
 
             <Col md={5}>
               <div className="new-leave-request-date-left-container">
-                <span>Fin</span>
-                {/*<LeaveCalendar onSelectDate={this.handleDateTo.bind(this)} startDate={moment().add(1, 'days')} />*/}
+                <span>Retorno</span>
                  <LeaveCalendar onSelectDate={this.handleDateTo.bind(this)} startDate={this.state.dateFrom.add(1, 'days')} />
               </div>
             </Col>
@@ -108,6 +126,11 @@ export default class NewLeaveRequest extends Component {
                   intelligentsia helvetica culpa next level eu cronut street art kitsch sint vegan. Readymade scenester
                   meditation consequat et cillum fixie velit gastropub dolore gentrify palo santo listicle literally semiotics. </p>
 
+            </Col>
+            <Col md={12}>
+              <div className="new-leave-request-approver">
+                <Select name="form-field-name" value={this.state.approvalPerson} options={this.state.options} onChange={this.onSelectChange}/>
+              </div>
             </Col>
           </div>
           <div className="new-leave-request-label-center-container">
@@ -130,4 +153,3 @@ export default class NewLeaveRequest extends Component {
     );
   }
 }
-
