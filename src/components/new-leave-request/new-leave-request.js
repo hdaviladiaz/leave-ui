a9a70f8c412f4ae885a2fa3218128b8a9ec87750
Roberto <rcordova@thoughtworks.com>
2017-06-27 12:58:20 -0500

    [#5] Validation for New Leave Request

--- src/components/new-leave-request/new-leave-request.js
+++ src/components/new-leave-request/new-leave-request.js
@@ -1,64 +1,75 @@
-import React, {Component} from 'react';
+import React, { Component } from 'react';
 import { Panel, Col } from 'react-bootstrap';
 import LeaveCalendar from '../leave-calendar/leave-calendar';
-import moment from 'moment';
 import LeaveRequestService from '../../services/leaveRequestService'
 import './new-leave-request.css';
-import { Alert } from 'react-bootstrap';
 import AlertContainer from 'react-alert';
-
-const date = moment().add(1, 'days');
+import moment from 'moment';
 
 export default class NewLeaveRequest extends Component {
 
   alertOptions = {
-   offset: 14,
-   position: 'bottom left',
-   theme: 'dark',
-   time: 5000,
-   transition: 'scale'
- }
+    offset: 14,
+    position: 'bottom left',
+    theme: 'dark',
+    time: 5000,
+    transition: 'scale'
+  }
 
- showAlert = () => {
-     this.msg.show('La fecha de inicio no puede ser mayor a la fecha de fin.', {
-       time: 2000,
-       type: 'success'
-     })
-   }
+  showAlert = (message) => {
+    this.msg.show(message, {
+      time: 3000,
+      type: 'success'
+    })
+  }
 
   constructor(props) {
 
     super(props),
-    this.state = {
-      dateFrom: moment(),
-      dateTo: date,
-      haveInformed: false
-    },
-    this.saveLeaveRequest = this.saveLeaveRequest.bind(this);
+      this.state = {
+        dateFrom: moment(),
+        dateTo: moment(),
+        haveInformed: false
+      },
+      this.saveLeaveRequest = this.saveLeaveRequest.bind(this);
     this.toggleInformed = this.toggleInformed.bind(this);
     this.leaveRequestService = LeaveRequestService.getInstance();
     this.showAlert = this.showAlert.bind(this);
   }
 
   handleDateFrom(dateFrom) {
-    this.setState({dateFrom:dateFrom});
+    this.setState({ dateFrom: dateFrom });
   }
 
   handleDateTo(dateTo) {
-    this.setState({dateTo:dateTo});
+    this.setState({ dateTo: dateTo });
   }
 
-  toggleInformed(){
-    this.setState({haveInformed: !this.state.haveInformed})
+  toggleInformed() {
+    this.setState({ haveInformed: !this.state.haveInformed })
   }
 
-  saveLeaveRequest(){
+  saveLeaveRequest() {
+ 
+    if (!this.state.haveInformed) {
+      this.showAlert('Por favor informe a su equipo y su PM y seleccione la opción.');
+      return;
+    } 
 
-  if (this.state.dateFrom > this.state.dateTo) {
-    this.showAlert()
-     } else {
-        this.leaveRequestService.createLeaveRequest({ employee_id: 99, start_date: this.state.dateFrom, end_date:this.state.dateTo, return_date: this.state.dateTo});
-    }
+    if (this.state.dateFrom > this.state.dateTo) {
+      this.showAlert('La fecha de inicio no puede ser mayor a la fecha de fin.');
+      return;
+    } 
+
+      this.leaveRequestService
+        .createLeaveRequest({ employee_id: 99, start_date: this.state.dateFrom, end_date: this.state.dateTo, return_date: this.state.dateTo })
+        .then(
+        function (response) {
+            window.location = "/";
+        })
+        .catch(function (error) {
+         });
+    
   }
 
   render() {
@@ -95,25 +106,26 @@ export default class NewLeaveRequest extends Component {
                   intelligentsia helvetica culpa next level eu cronut street art kitsch sint vegan. Readymade scenester
                   meditation consequat et cillum fixie velit gastropub dolore gentrify palo santo listicle literally semiotics. </p>
 
-              </Col>
-            </div>
-            <div className="new-leave-request-label-center-container">
-              <Col md={12}>
-                <input type="checkbox" value="" defaultChecked={this.state.haveInformed} onChange={this.toggleInformed}/>
-                <label>&nbsp; He confirmado a mi equipo y mi PM </label>
-              </Col>
-            </div>
-            <div className="new-leave-request-button-center-container">
-              <Col md={5}/>
-                <Col  md={2}>
-                  <AlertContainer ref={a => this.msg = a} {...this.alertOptions} />
-                  <button className="buttonSend" onClick={this.saveLeaveRequest}>ENVIAR</button>
-                </Col>
-              <Col md={5}/>
-            </div>
-          </Panel>
+            </Col>
+          </div>
+          <div className="new-leave-request-label-center-container">
+            <Col md={12}>
+              <input type="checkbox" value="" defaultChecked={this.state.haveInformed} onChange={this.toggleInformed} />
+              <label>&nbsp; He confirmado a mi equipo y mi PM </label>
+            </Col>
+          </div>
+          <div className="new-leave-request-button-center-container">
+            <Col md={5} />
+            <Col md={2}>
+              <AlertContainer ref={a => this.msg = a} {...this.alertOptions} />
+              <button className="buttonSend" onClick={this.saveLeaveRequest.bind(this)}>ENVIAR</button>
+            </Col>
+            <Col md={5} />
+          </div>
+        </Panel>
       </div>
 
     );
   }
 }
+
