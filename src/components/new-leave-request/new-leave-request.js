e6ce378bb5571599461b6bf2927584c1f586cf40
Schubert D. Rodríguez <scrodrig@thoughtworks.com>
2017-06-29 16:02:14 -0500

    [#32] - Add a Loading Spinner in order to prevent multiple request

--- src/components/new-leave-request/new-leave-request.js
+++ src/components/new-leave-request/new-leave-request.js
@@ -4,6 +4,7 @@ import LeaveCalendar from '../leave-calendar/leave-calendar';
 import LeaveRequestService from '../../services/leaveRequestService'
 import PeopleService from '../../services/peopleService';
 import './new-leave-request.css';
+import Loader from 'react-loader';
 import AlertContainer from 'react-alert';
 import moment from 'moment';
 import Select from 'react-select';
@@ -25,6 +26,28 @@ export default class NewLeaveRequest extends Component {
     transition: 'scale'
   }
 
+  loaderOptions = {
+      lines: 13,
+      length: 20,
+      width: 10,
+      radius: 30,
+      scale: 1.00,
+      corners: 1,
+      color: '#000',
+      opacity: 0.25,
+      rotate: 0,
+      direction: 1,
+      speed: 1,
+      trail: 60,
+      fps: 20,
+      zIndex: 2e9,
+      top: '50%',
+      left: '50%',
+      shadow: false,
+      hwaccel: false,
+      position: 'absolute'
+  }
+
   showAlert = (message) => {
     this.msg.show(message, {
       time: 3000,
@@ -34,14 +57,15 @@ export default class NewLeaveRequest extends Component {
 
   constructor(props) {
     super(props),
-      this.state = {
-        dateFrom: moment(),
-        dateTo: moment().add(1, 'days'),
-        haveInformed: false,
-        approvalPerson: {},
-        options: []
-      },
-      this.peopleService = PeopleService.getInstance();
+    this.state = {
+      dateFrom: moment(),
+      dateTo: moment().add(1, 'days'),
+      haveInformed: false,
+      approvalPerson: {},
+      loaded: true,
+      options: []
+    },
+    this.peopleService = PeopleService.getInstance();
     this.saveLeaveRequest = this.saveLeaveRequest.bind(this);
     this.toggleInformed = this.toggleInformed.bind(this);
     this.leaveRequestService = LeaveRequestService.getInstance();
@@ -81,34 +105,38 @@ export default class NewLeaveRequest extends Component {
   }
 
 
-  saveLeaveRequest() {
-    let custom = this;
+    this.setState({loaded: false});
+
     if (!this.state.haveInformed) {
-      this.showAlert('Por favor informe a su equipo y su líder de proyecto y seleccione la opción.');
+      this.showAlert('Por favor informe a su equipo y su PM y seleccione la opción.');
+      this.setState({loaded: true});
       return;
     }
 
     if (this.state.approvalPerson == null || this.state.approvalPerson.value === undefined) {
       this.showAlert('Por favor seleccione un aprobador.');
+      this.setState({loaded: true});
       return;
     }
 
     if (this.state.dateFrom.isAfter(this.state.dateTo) || this.state.dateFrom.isSame(this.state.dateTo)) {
       this.showAlert('La fecha de inicio no puede ser mayor o igual a la fecha de retorno.');
+      this.setState({loaded: true});
       return;
     }
 
-    this.leaveRequestService.createLeaveRequest({ start_date: this.state.dateFrom, end_date: this.state.dateTo, return_date: this.state.dateTo, approver_id: this.state.approvalPerson.value })
-      .then(function (response) {
-        custom.context.router.history.push('/dashboard/leaves');
-      }).catch(function (error) {
-        console.log(error);
-      });
+    this.leaveRequestService.createLeaveRequest({start_date: this.state.dateFrom, end_date: this.state.dateTo, return_date: this.state.dateTo, approver_id: this.state.approvalPerson.value}).then(() => {
+      this.setState({loaded: true});
+      window.location = "/dashboard/leaves";
+    }).catch(function(error) {
+      console.log(error);
+    });
   }
 
   render() {
     return (
       <div>
+
         <Panel className="leave-request">
           <h3>Solicitud de vacaciones</h3>
           <div className="new-leave-request-container">
@@ -121,7 +149,7 @@ export default class NewLeaveRequest extends Component {
 
             <Col md={2}>
               <div>
-                <img alt="" className="new-leave-request-date-center-container" src={"/img/leave-line.png"} />
+                <img alt="" className="new-leave-request-date-center-container" src={"/img/leave-line.png"}/>
               </div>
             </Col>
 
@@ -154,16 +182,18 @@ export default class NewLeaveRequest extends Component {
               </label>
             </Col>
           </div>
-
-          <div className="new-leave-request-button-center-container">
-            <Col md={5} />
-            <Col md={2}>
-              <AlertContainer ref={a => this.msg = a} {...this.alertOptions} />
-              <button className="buttonSend" onClick={this.saveLeaveRequest.bind(this)}>ENVIAR</button>
-            </Col>
-            <Col md={5} />
-          </div>
+          <Loader loaded={this.state.loaded} options={this.loaderOptions}>
+            <div className="new-leave-request-button-center-container">
+              <Col md={5}/>
+              <Col md={2}>
+                <AlertContainer ref={a => this.msg = a} {...this.alertOptions}/>
+                <button className="buttonSend" onClick={this.saveLeaveRequest.bind(this)}>ENVIAR</button>
+              </Col>
+              <Col md={5}/>
+            </div>
+          </Loader>
         </Panel>
+
       </div>
 
     );
